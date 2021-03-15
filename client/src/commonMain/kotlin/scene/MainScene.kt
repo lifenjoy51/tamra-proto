52739d32cdac7100ce2bc787b86fcd1f23b35409
joey.51 <joey.51@kakaocorp.com>
2021-03-16 01:29:29 +0900

    feat: soft keyboard 개발. 메인화면 아이디 입력.

--- client/src/commonMain/kotlin/scene/MainScene.kt
+++ client/src/commonMain/kotlin/scene/MainScene.kt
@@ -8,35 +8,64 @@ import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korio.lang.substr
 import scene.world.WorldScene
 import tamra.mainHeight
 import tamra.mainWidth
+import ui.BACKSPACE
+import ui.keyboard
+import ui.tamraRect
 import ui.tamraText
 
 class MainScene : Scene() {
     override suspend fun Container.sceneInit() {
-        // background
-        solidRect(mainWidth, mainHeight, Colors.LIGHTBLUE) {}
 
-        // title
-        tamraText("탐라", color = Colors.DARKBLUE, textSize = 96.0, py = 64) {
+        // background
+        solidRect(mainWidth, mainHeight, Colors.DARKGREY)
+
+        // logo
+        val logo = sprite(texture = resourcesVfs["L200.png"].readBitmap()) {
+            positionY(mainHeight * 1 / 5)
             centerXOnStage()
         }
 
-        // logo
-        sprite(texture = resourcesVfs["L200.png"].readBitmap()) {
-            centerOnStage()
+        val idText = tamraText("ID를 입력하세요.", color = Colors.BLACK) {
+            alignTopToBottomOf(logo, 30)
+            centerXOnStage()
+        }
+
+        val idInput = tamraText("", color = Colors.BLACK) {
+            alignTopToBottomOf(idText, 20)
+            centerXOnStage()
         }
 
         // start
-        tamraText("Touch to Start", textSize = 36.0, color = Colors.DARKBLUE, py = mainHeight * 2 / 3) {
+        fixedSizeContainer(200, 40) {
+            alignTopToBottomOf(idInput, 50)
             centerXOnStage()
-            alpha = 0.8
-            onOut { alpha = 0.8 }
-            onOver { alpha = 1.0 }
-            onClick {
-                sceneContainer.changeTo<WorldScene>()
+            val bg = tamraRect(width, height, color = Colors.DIMGREY)
+            tamraText("시작하기", textSize = 24.0, color = Colors.WHITE) {
+                centerOn(bg)
             }
+            tamraRect(width, height, color = Colors.TRANSPARENT_WHITE) {
+                alpha = 0.1
+                onOut { alpha = 0.1 }
+                onOver { alpha = 0.5 }
+                onClick {
+                    sceneContainer.changeTo<WorldScene>()
+                }
+            }
+        }
+
+        keyboard {
+            if (it == BACKSPACE) {
+                if (idInput.text.isNotEmpty()) {
+                    idInput.text = idInput.text.substr(0, idInput.text.length - 1)
+                }
+            } else {
+                idInput.text += it
+            }
+            idInput.centerXOnStage()
         }
     }
 }
